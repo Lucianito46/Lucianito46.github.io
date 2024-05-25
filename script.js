2a1e2aed411a0d4cacebd87eb72da0d126817351
Luciano Gatica <lucianogatica542@gmail.com>
2024-05-25 00:24:47 -0300

    arreglando un mensaje de error

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-var swiper = new Swiper(".mySwiper-1", {
-    slidesPerView:1,
+var swiper1 = new Swiper(".mySwiper-1", {
+    slidesPerView: 1,
     spaceBetween: 30,
-    loop:true,
+    loop: true,
     pagination: {
-        el:".swiper-pagination",
-        clickable:true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
-        nextEl:".swiper-button-next",
-        prevEl:".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     }
 });
 
-var swiper = new Swiper(".mySwiper-2", {
-    slidesPerView:3,
+var swiper2 = new Swiper(".mySwiper-2", {
+    slidesPerView: 3,
     spaceBetween: 20,
-    loop:true,
-    loopFillGropWithBlank:true,
+    loop: true,
+    loopFillGroupWithBlank: true,
     navigation: {
-        nextEl:".swiper-button-next",
-        prevEl:".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-    breakpoinsts: {
+    breakpoints: {
         0: {
-            slidesPerView:1,
+            slidesPerView: 1,
         },
         520: {
-            slidesPerView:2,
+            slidesPerView: 2,
         },
         950: {
-            slidesPerView:3,
+            slidesPerView: 3,
         },
     }
 });
@@ -37,10 +37,10 @@ var swiper = new Swiper(".mySwiper-2", {
 let tabInputs = document.querySelectorAll(".tabInput");
 
 tabInputs.forEach(function(input){
-
-    input.addEventListener('change',function(){
-        let id = input.ariaValueMax;
-        let thisSwiper = document.getElementById('swiper'+ id);
+    input.addEventListener('change', function(){
+        let id = input.getAttribute('aria-valuemax');
+        let thisSwiper = document.getElementById('swiper' + id);
         thisSwiper.swiper.update();
-    })
+    });
 });
+
